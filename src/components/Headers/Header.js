e482e9f85e8b1ac974753f7816a294cf48b1a328
tensoradam <adamfarooq07@gmail.com>
2025-09-30 08:40:59 +0500

    Refactor Header.js

---
+++ src/components/Headers/Header.js
@@ -0,0 +1,17 @@
+// reactstrap components
+import { Container} from "reactstrap";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header bg-gradient-info pb-6 pt-5 pt-md-8">
+        <Container fluid>
+          <div className="header-body">
+           </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Header;
