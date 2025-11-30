4803bc102d187e3133c0ac7df127eac44f4e8d62
tensoradam <adamfarooq07@gmail.com>
2025-11-30 16:51:57 +0500

    Refactor AdminFooter.js

---
+++ src/components/Footers/AdminFooter.js
@@ -0,0 +1,31 @@
+/*eslint-disable*/
+
+// reactstrap components
+import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Row className="align-items-center justify-content-xl-between">
+        <Col xl="6">
+          <div className="copyright text-center text-xl-left text-muted">
+            © {new Date().getFullYear()}{" "}
+            <a
+              className="font-weight-bold ml-1"
+              href="#"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              TCS
+            </a>
+          </div>
+        </Col>
+
+        <Col xl="6">
+        </Col>
+      </Row>
+    </footer>
+  );
+};
+
+export default Footer;
